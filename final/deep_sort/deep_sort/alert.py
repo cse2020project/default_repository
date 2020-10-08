ab13e3c19a10789e3e6d1f16fb25915393f8558f
ysy9893 <gptjddlthdus123@gmail.com>
2020-10-08 19:08:15 +0900

    CHANGED alert,deepsort,coor,track

---
+++ final/deep_sort/deep_sort/alert.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def show_direction(ax,theta,angle,frame,identities):
+
+    #좌표 plot
+    for thetha_single in theta:
+        r = np.ones(np.shape(thetha_single))
+        ax.quiver(thetha_single, r, np.pi, -1, color='black', angles="xy", scale_units='xy', scale=1.)
+        ax.plot(thetha_single, r, color='black', marker='o', markersize=5)
+
+
+
+
+
+
+
+
+
+
+
+
