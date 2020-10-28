ab67d701547263f663aa41b2000ee2a755be576c
eun-g-kim <43198923+eun-g-kim@users.noreply.github.com>
2020-10-28 19:29:54 +0900

    Update deep_sort.py

--- deep_sort/deep_sort/deep_sort.py
+++ deep_sort/deep_sort/deep_sort.py
@@ -1,5 +1,6 @@
 import numpy as np
 import torch
+import math
 
 from .deep.feature_extractor import Extractor
 from .sort.nn_matching import NearestNeighborDistanceMetric
@@ -8,7 +9,7 @@ from .sort.detection import Detection
 from .sort.tracker import Tracker
 
 from . import coor
-from . import alert
+
 
 __all__ = ['DeepSort']
 
@@ -25,12 +26,16 @@ class DeepSort(object):
         metric = NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
         self.tracker = Tracker(metric, max_iou_distance=max_iou_distance, max_age=max_age, n_init=n_init)
 
-    def update(self, bbox_xywh, confidences, ori_img, dict):
+    def update(self, bbox_xywh, confidences, ori_img, dict,frame):
+
+
+
         self.height, self.width = ori_img.shape[:2]
         # generate detections
         features = self._get_features(bbox_xywh, ori_img)
         bbox_tlwh = self._xywh_to_tlwh(bbox_xywh)
-        detections = [Detection(bbox_tlwh[i], conf, features[i]) for i,conf in enumerate(confidences) if conf>self.min_confidence]
+        detections = [Detection(bbox_tlwh[i], conf, features[i]) for i, conf in enumerate(confidences) if
+                      conf > self.min_confidence]
 
         # run on non-maximum supression
         boxes = np.array([d.tlwh for d in detections])
@@ -42,9 +47,12 @@ class DeepSort(object):
         self.tracker.predict()
         self.tracker.update(detections)
 
-
         # coordinate - 방향 전달
         coors = []
+        #각도전달
+        angle=[]
+        #아이디 전달
+        identities=[]
 
         # output bbox identities
         outputs = []
@@ -52,47 +60,40 @@ class DeepSort(object):
             if not track.is_confirmed() or track.time_since_update > 1:
                 continue
             box = track.to_tlwh()
-            x1,y1,x2,y2 = self._tlwh_to_xyxy(box)
+            x1, y1, x2, y2 = self._tlwh_to_xyxy(box)
 
-            box_ratio = (x2-x1)/(y2-y1)
-            box_size = (x2-x1)*(y2-y1)
+            x_center = (x2+x1)/2
+            box_ratio = (x2 - x1) / (y2 - y1)
+            box_size = (x2 - x1) * (y2 - y1)
             track_id = track.track_id
 
-            # 0단계: 위험거리보다 멀리 있는 차량을 필터링
-            if box_size < 600: continue
-            #    print("filter0 ")
-            #    continue
-            # 1단계: 가로세로 비율을 비교해 측면차량을 필터링
-            if box_ratio > 1.8: continue
-            #    print("filter1 ")
-            #    continue
-            # 2단계: 이전 프레임보다 바운딩박스가 작아진, 즉 멀어지는 차량을 필터링 (그대로인 차량도 필터링)
-            #if track_id in dict and dict[track_id] >= box_size : continue #dict에 id 정보가 이미 있어야함
-            ####여기 밑에 두줄은 bbox 확인용으로 만든것. 나중에 완성후에는 위 코드로 바꾸면 됨.
-            if track_id in dict and dict[track_id] >= box_size:
-                isCloser = False
-            #    print("filter2 ")
-                continue
-            isCloser = False #아직 dict에 없는 애들을 위해서
+            isCloser = True
+            if track_id not in dict : isCloser = False
+            # 1단계: 위험거리보다 멀리 있는 차량을 필터링
+            if box_size < 1000: isCloser=False
+            # 2단계: 가로세로 비율을 비교해 측면차량을 필터링
+            if box_ratio > 1.5: isCloser=False
+            # 3단계: 주차된 차량을 필터링
+            if track_id in dict and abs(dict[track_id][0]-x_center) > 2.5: isCloser = False #x좌표가 +-2.5보다 크면 주차된차 (사람움직)
+            if track_id in dict and abs((dict[track_id][1]-box_size)/dict[track_id][1]) < 0.055: isCloser = False #바운딩박스 크기가 오차포함 일정하면 주차(사람 멈춤)
+            if track_id in dict and dict[track_id][1]==False: isCloser = False
+            # 4단계: 이전 프레임보다 바운딩박스가 작아진, 즉 멀어지는 차량을 필터링
+            if track_id in dict and dict[track_id][2] > box_size : isCloser = False #dict에 id 정보가 이미 있어야함
 
-            if track_id in dict and dict[track_id] < box_size : #dict에 id 정보가 이미 있고 박스 크기가 커졌다면
-                isCloser = True
-                print("\ncar ", track_id ,"\'s degree is: ", coor.coor((x2 + x1) / 2)*57.29578)
-                print(x1, x2, (x1+x2)/2)
-                coors.append(coor.coor(x2+x1)/2) #중점좌표의 라디안값 저장
+            # 필터링 끝났는데 남아있으면 다가오는 차량
+            if isCloser == True :
+                print("\ncar ", track_id, "\'s degree is: ", coor.coor((x2 + x1) / 2) * 57.29578)
+            coors.append(coor.coor(x2 + x1) / 2)  # 중점좌표의 라디안값 저장
 
-            #outputs.append(np.array([x1, y1, x2, y2, track_id], dtype=np.int))
-            outputs.append(np.array([x1, y1, x2, y2, track_id, isCloser], dtype=np.int)) ####outputs도 bbox확인용으로 만든것것
-            dict[track_id] = box_size
+            outputs.append(np.array([x1, y1, x2, y2, track_id, isCloser], dtype=np.int))  ####outputs도 bbox확인용으로 만든것것
+            dict[track_id] = [x_center, box_size, isCloser, box_ratio]
 
         if len(outputs) > 0:
-            outputs = np.stack(outputs,axis=0)
+            outputs = np.stack(outputs, axis=0)
 
-        # 방향 display하는 함수 호출
-        #if len(coors)>0:
-        #    alert.show_direction(coors)
-        return outputs
 
+
+        return outputs,coors,frame
 
     """
     TODO:
